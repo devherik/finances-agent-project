7eaad684c47eff016dd807e810359ccf3e2df06d
Herik Colares Rezende <dev.herik@gmail.com>
2025-09-29 06:12:22 -0300

    feat: enhance contact message use case with MongoDB integration; implement user existence check and user creation logic

--- usecases/contact_message_use.py
+++ usecases/contact_message_use.py
@@ -4,42 +4,92 @@ It will make search in the database for the user information and return it if it
 Also, it will 'map' the user message, finding the user intent, with means every time a message arrives to the webhook, this use case will be triggered.
 """
 
+from pymongo.database import Database
+
 from models.models import User
 from typing import Optional
-from core.factories import create_mongo_db, create_agents_service
+from core.factories import create_agents_service
+from services.mongodb_manager import MongoDbManager
+from services.agent_service import AgentsService
+from repositories.mongodb_repository import MongoDBRepository
+from helpers.loging_helper import logger
 
 
 class ContactMessageUse:
     def __init__(self):
-        
-        pass
+        try:
+            self.db_manager = MongoDbManager()
+            self.mongodb_repository = MongoDBRepository(self.db_manager.get_database())
+            self.agents_service: AgentsService = create_agents_service()
+        except Exception as e:
+            logger.error(f"Error initializing ContactMessageUse: {e}")
+            return None
 
     async def filter_contact_message(self, phone_number: str, message: str) -> bool:
         # Here we will implement the logic to validate the contact information.
         # We will use a Intent Recognition model to identify the user intent.
         # The phone number will be used as the unique identifier for the user (session_id).
         # For now, we will just return True if the message contains "link" or "create"
+
         try:
-            if not await self.is_user_existing(phone_number):
-                new_user = User(phone=phone_number)
-                # Here we would save the new user to the database
+            user = await self.is_user_existing(phone_number)
+
+            if user:
+                logger.debug(f"User found: {user}")
+                # Proceed with linking or other actions for existing user
+                return True
             else:
-                return False
+                logger.info("No existing user found, creating new user.")
+                new_user = await self.create_new_user(phone_number)
+                if new_user:
+                    logger.debug(f"New user created: {new_user}")
+                    # Proceed with onboarding actions for new user
+                    return True
+                else:
+                    logger.error("Failed to create new user.")
+                    return False
         except Exception as e:
+            logger.error(f"Error filtering contact message: {e}")
             return False
-        return False
 
     async def is_user_existing(self, phone_number: str) -> User | None:
         # Here we will implement the logic to check if the user exists in the database.
         # For now, we will just return False to simulate that the user does not exist.
         user: User | None = None
+
         try:
-            # Simulate database lookup
-            user = User(phone=phone_number) # Replace with actual DB call
+            user: User | None = await self.mongodb_repository.get_user_by_phone(
+                phone_number
+            )
+
             if not user:
                 user = User(phone=phone_number)
-                # Here we would save the new user to the database
+                id = await self.mongodb_repository.add_user(user)
+
+                if id:
+                    user.id = str(id)
+                    logger.debug(f"New user created with ID: {user.id}")
+                else:
+                    logger.error("Failed to create new user.")
+                    return None
             return user
         except Exception as e:
-            ...
+            logger.error(f"Error checking if user exists: {e}")
         return None
+
+    async def create_new_user(self, phone_number: str) -> Optional[User]:
+        
+        try:
+            new_user = User(phone=phone_number)
+            user_id = await self.mongodb_repository.add_user(new_user)
+
+            if user_id:
+                new_user.id = str(user_id)
+                logger.debug(f"New user created with ID: {new_user.id}")
+                return new_user
+            else:
+                logger.error("Failed to create new user.")
+                return None
+        except Exception as e:
+            logger.error(f"Error creating new user: {e}")
+            return None
