7cca6e87881905dff8ff2c8662ebbbef6aeb8cbe
Herik Colares Rezende <dev.herik@gmail.com>
2025-09-29 20:51:27 -0300

    refactor: remove unused imports and improve error logging in first contact workflow

--- workflows/first_contact_workflow.py
+++ workflows/first_contact_workflow.py
@@ -9,7 +9,7 @@ from typing import List, Any
 
 from core.interfaces import IWorkflow, ISenderMessage
 from services.mockup_sender import MockupSender
-from models.models import User, Account, NewUser
+from helpers.loging_helper import logger
 
 class FirstContactWorkflow(IWorkflow):
     def __init__(self, session_id: str, sender: ISenderMessage = MockupSender()):
@@ -22,7 +22,6 @@ class FirstContactWorkflow(IWorkflow):
 
     async def arun(self, phone_number: str | None, initial_message: str) -> str:
         
-        # 1. Greet the user and introduce the agent
         inicial_messages = [
             "Hello! I'm your Finances Agent, here to help you manage your finances effectively.",
             "First, I need to gather some information to get started.\n",
@@ -35,9 +34,4 @@ class FirstContactWorkflow(IWorkflow):
                 body="\n".join(inicial_messages)
             )
         except Exception as e:
-            print(f"Failed to send welcome message: {e}")
-
-
-        # 2. Ask for the user's name and email if not already provided
-        # 3. Confirm the user's identity if information is found in the database
-        # 4. Ask if the user wants to link any existing accounts or create new ones
+            logger.error(f"Failed to send welcome message: {e}")
